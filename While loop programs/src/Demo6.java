905fa02b734e8b329ac54015d828d21d76a6c154
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-09 06:32:28 +0530

    Looping statement practice programs

---
+++ While loop programs/src/Demo6.java
@@ -0,0 +1,12 @@
+//Demo on while loop
+public class Demo6 {
+	public static void main(String[] args) {
+	int i = 1;
+	while(i<=10)
+	{
+	System.out.println(i);
+	i++;
+	}
+	System.out.println("process done");
+	}
+}
---
+++ do-while program/src/Demo7.java
@@ -0,0 +1,12 @@
+//Demo on do-while
+public class Demo7 {
+	public static void main(String[] args) {
+	int i = 1;
+	do
+	{
+	System.out.println(i);
+	i++;
+	}
+	while(i<=10);
+	}
+}
